bc6998826ec11a7b55b42a3d9dc26ff4b3c97bb8
Yeo Yu Quan <yuquanyeo@gmail.com>
2021-04-03 20:03:42 +0800

    Fake Data for Database
    Data faker than my ass

--- backend/payment/payment.py
+++ backend/payment/payment.py
@@ -18,7 +18,7 @@ CORS(app)
 class Payment(db.Model):
     __tablename__ = 'payment'
 
-    payment_id = db.Column(db.Integer, primary_key=True)
+    payment_id = db.Column(db.String(100), nullable=False, primary_key=True)
     liaise_id = db.Column(db.Integer, nullable=False)
     sender_id = db.Column(db.Integer, nullable=False)
     receiver_id = db.Column(db.Integer, nullable=False)
